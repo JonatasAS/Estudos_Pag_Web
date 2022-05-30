c708eb665fdc529fdc802e8b791422235dc2f9ed
Jonatas_Amorim <jonatas231997@live.com>
2022-05-30 15:11:46 -0300

    More calculator functions

--- Projetos_JS/EstudosComJS/script.js
+++ Projetos_JS/EstudosComJS/script.js
@@ -4,6 +4,7 @@ let secondNumber = prompt("Informe o segundo número: ")
 firstNumber = Number(firstNumber)
 secondNumber = Number(secondNumber)
 
+
 const sum = firstNumber + secondNumber
 const sub = firstNumber - secondNumber
 const mult = firstNumber * secondNumber
@@ -15,3 +16,17 @@ alert("Subtração " + sub)
 alert("Multiplicação " + mult )
 alert("Divisão " + div.toFixed(1))
 alert("Resto da divisão " + restDiv)
+
+if(restDiv == 0){
+    alert("O resto da divisão e par!")
+}
+else {
+    alert("O resto da divisão e impar!")
+}
+
+if (firstNumber != secondNumber){
+    alert("Os dois números são diferentes!")
+}
+else {
+    alert("Os dois números são iguais!")
+}
