7700d9a99c40b6404b7b8608f196099578d0c19e
JonatasAS <jonatas231997@live.com>
2022-05-30 13:33:05 -0300

    Exercise 01 calculator

---
+++ Projetos_JS/EstudosComJS/script.js
@@ -0,0 +1,17 @@
+let firstNumber = prompt("Informe o primeiro número: ")
+let secondNumber = prompt("Informe o segundo número: ")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const mult = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert("Soma " + sum)
+alert("Subtração " + sub)
+alert("Multiplicação " + mult )
+alert("Divisão " + div)
+alert("Resto da divisão " = restDiv)
