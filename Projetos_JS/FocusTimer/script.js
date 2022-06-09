24686ae3ae00aa25d3ddc6826f90caa66b8a35fe
JonatasAS <jonatas231997@live.com>
2022-06-09 14:04:57 -0300

    Function FocusTimer

--- Projetos_JS/FocusTimer/script.js
+++ Projetos_JS/FocusTimer/script.js
@@ -9,13 +9,30 @@ let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
-function countdown(){
-  setTimeout(function() {
+function countdown() {
+  setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
-    if (seconds <= 0) {
-      seconds = 60
+    let minutes = Number(minutesDisplay.textContent)
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+
+      return
     }
-    secondsDisplay.textContent = seconds - 1
+
+    if (seconds <= 0) {
+      seconds = 2
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
     countdown()
   }, 1000)
 }
@@ -51,7 +68,7 @@ buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
 })
 
-buttonSet.addEventListener('click', function() {
+buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
 })
