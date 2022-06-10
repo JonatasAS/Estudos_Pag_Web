a471c5fd5f2e2522f1eb496b56efd412f55865eb
JonatasAS <jonatas231997@live.com>
2022-06-10 13:13:57 -0300

    Module

--- Projetos_JS/FocusTimer/js/index.js
+++ Projetos_JS/FocusTimer/js/index.js
@@ -1,3 +1,6 @@
+import resetControls from "./controls"
+import "./timer.js"
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
@@ -9,47 +12,6 @@ const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimerOut
-
-function resetControls() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
-
-}
-
-function resetTime() {
-  updateTimeDisplay(minutes, 0)
-  clearTimeout(timerTimerOut)
-}
-
-function updateTimeDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes).padStart(2, "0")
-  secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function countdown() {
-  timerTimerOut = setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent)
-    let minutes = Number(minutesDisplay.textContent)
-
-    updateTimeDisplay(minutes, 0)
-
-    if (minutes <= 0) {
-      resetControls()
-      return
-    }
-
-    if (seconds <= 0) {
-      seconds = 2
-      --minutes
-    }
-
-    updateTimeDisplay(minutes, String(seconds - 1))
-
-    countdown()
-  }, 1000)
-}
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
---
+++ Projetos_JS/FocusTimer/js/timer.js
@@ -0,0 +1,34 @@
+function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetTime() {
+    updateTimeDisplay(minutes, 0)
+    clearTimeout(timerTimerOut)
+}
+
+function countdown() {
+    timerTimerOut = setTimeout(function () {
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        updateTimeDisplay(minutes, 0)
+
+        if (minutes <= 0) {
+            resetControls()
+            return
+        }
+
+        if (seconds <= 0) {
+            seconds = 2
+            --minutes
+        }
+
+        updateTimeDisplay(minutes, String(seconds - 1))
+
+        countdown()
+    }, 1000)
+}
+
+console.log('timer')
