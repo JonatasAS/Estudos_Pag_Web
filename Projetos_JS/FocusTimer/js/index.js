d29a021f7c1350402bff6c461af0c9a512577533
Jonatas_Amorim <jonatas231997@live.com>
2022-06-13 20:12:51 -0300

    Module events

--- Projetos_JS/FocusTimer/js/index.js
+++ Projetos_JS/FocusTimer/js/index.js
@@ -2,6 +2,7 @@
 import Controls from "./controls.js"
 import  Timer  from "./timer.js"
 import Sound from "./sounds.js"
+import Event from "./events.js"
 import {
   buttonPlay,
   buttonPause,
@@ -28,43 +29,5 @@ const timer = Timer({
 
 const sound = Sound()
 
-buttonPlay.addEventListener('click', function () {
-  controls.play()
-  timer.countdown()
-  sound.pressButton()
-})
+Event({controls, timer, sound})
 
-buttonPause.addEventListener('click', function () {
-  controls.pause()
-  timer.hold()
-  sound.pressButton()
-})
-
-buttonStop.addEventListener('click', function () {
-  controls.reset()
-  timer.reset()
-  sound.pressButton()
-})
-
-buttonSoundOff.addEventListener('click', function () {
-  buttonSoundOn.classList.remove('hide')
-  buttonSoundOff.classList.add('hide')
-  sound.bgAudio.pause()
-})
-
-buttonSoundOn.addEventListener('click', function () {
-  buttonSoundOff.classList.remove('hide')
-  buttonSoundOn.classList.add('hide')
-  sound.bgAudio.play()
-})
-
-buttonSet.addEventListener('click', function () {
-  let newMinutes = controls.getMinutes()
-  if (!newMinutes) {
-    timer.reset()
-    return
-  }
-
-  timer.updateDisplay(newMinutes, 0)
-  timer.updateMinutes(newMinutes)
-})
