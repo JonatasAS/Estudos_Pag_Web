f7c46b8c4e07d0af902a9189210819e567ee6040
Jonatas_Amorim <jonatas231997@live.com>
2022-06-16 21:27:50 -0300

    conifg buttons

---
+++ Projetos_JS/FocusTimer2/js/events.js
@@ -0,0 +1,32 @@
+import {
+  buttonCoffee,
+  buttonFire,
+  buttonForrest,
+  buttonLess,
+  buttonPlay,
+  buttonPlus,
+  buttonRain,
+  buttonStop
+} from "./elements.js"
+
+export default function ({ timer, sound }) {
+
+  buttonPlay.addEventListener('click', function () {
+  })
+  buttonStop.addEventListener('click', function () {
+  })
+  buttonPlus.addEventListener('click', function () {
+  })
+  buttonLess.addEventListener('click', function () {
+  })
+  buttonForrest.addEventListener('click', function () {
+  })
+  buttonRain.addEventListener('click', function () {
+  })
+  buttonCoffee.addEventListener('click', function () {
+  })
+  buttonFire.addEventListener('click', function () {
+  })
+
+
+}
---
+++ Projetos_JS/FocusTimer2/js/index.js
@@ -0,0 +1,11 @@
+import Event from "./events.js";
+import {
+  buttonCoffee,
+  buttonFire,
+  buttonForrest,
+  buttonLess,
+  buttonPlay,
+  buttonPlus,
+  buttonRain,
+  buttonStop
+}from "./elements.js"
